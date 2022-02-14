7b6d3d37be92ea81952f928c735f274a7ff6b711
Andrei Androne <andrei.androne@outlook.com>
2022-02-14 19:08:50 +0200

    Oop_Constructors_1

---
+++ OopConstructors/OopConstructors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OopConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("John","Doe", new DateTime(1980, 2, 14));
+
+            Person p2 = new Person();
+
+            p.Print();
+            p2.Print();
+        }
+    }
+}
